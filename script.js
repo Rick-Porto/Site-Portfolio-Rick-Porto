b48944c567b22ff6737ca933abc765b90755b3ce
Rick Porto <rickmarco4@hotmail.com>
2023-07-04 18:31:31 -0300

    Typing in Tittle and Somes ajusts in CSS an JS

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function ativaLetra(elemento){
     const arrTexto = elemento.innerHTML.split('');
-    console.log(arrTexto)
+    elemento.innerHTML = '';
+    arrTexto.forEach((Letra, i) =>{
+        setTimeout(()=>{
+            elemento.innerHTML += Letra;
+        }, 75 * i);
+    });
 }
 
 const titulo = document.querySelector('.letreiro');
+ativaLetra(titulo);
+
