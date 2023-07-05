6814df1a174edd570a7fa272474b3ee1b8434ab5
Rick Porto <rickmarco4@hotmail.com>
2023-07-05 03:19:08 -0300

    Making Site responsive and Creating new Pages

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-function ativaLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((Letra, i) =>{
-        setTimeout(()=>{
-            elemento.innerHTML += Letra;
-        }, 75 * i);
-    });
+function digitando(){
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((Letra, i) =>{
+            setTimeout(()=>{
+                elemento.innerHTML += Letra;
+            }, 75 * i);
+        });
+    }
+
+    const titulo = document.querySelector('.letreiro');
+    ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.letreiro');
-ativaLetra(titulo);
+const ativaMenu = document.querySelector('.bi-list');
+    const navMenu = document.querySelector('header .menu')
+
+    ativaMenu.addEventListener('click', ()=>{
+        ativaMenu.classList.toggle('bi-x')
+        navMenu.classList.toggle('ativado')
+    })
+
 
